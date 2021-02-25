8531d67509351ab701e4a8e3bbd879d25538a6b5
Giovanni <giovannipor23@gmail.com>
2021-02-25 15:25:59 +0100

    Esercizio Completo (con While e Do-While)
    Logica e Console

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ Scrivi un programma che stampi i numeri da 1 a 100,
 ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz. */
 
+console.log("Esercizio con ciclo For");
+console.log("");
+
 //ciclo for per stampare 100 numeri
 for (i = 1; i < 101; i++) {
 
@@ -27,3 +30,69 @@ for (i = 1; i < 101; i++) {
         console.log(i);
     }
 }
+
+console.log("");
+console.log("Esercizio con ciclo While");
+console.log("");
+
+x = 1;
+
+while (x < 101) {
+
+    //se il numero è divisibile sia per 3 che per 5 (and), allora
+    if ((x % 3 == 0) && (x % 5 == 0)) {
+
+        //stampa "FizzBuzz"
+        console.log("FizzBuzz");
+
+    //altrimenti, se è divisibile per 3 ma NON per 5, allora
+    } else if ((x % 3 == 0) && (x % 5 != 0)) {
+        //stampa solo "Fizz"
+        console.log("Fizz");
+
+    //altriment, se NON è divisibile per 3 ma solo per 5, allora
+    } else if ((x % 3 != 0) && (x % 5 == 0)) {
+        //stampa solo "Buzz"
+        console.log("Buzz");
+    //se non rientra in nessuna delle condizioni sopra, allora
+    } else {
+        //stampa il valore dell'indice
+        console.log(x);
+    }
+
+    x++;
+
+}
+
+console.log("");
+console.log("Esercizio con ciclo Do-While");
+console.log("");
+
+y = 1;
+
+do {
+
+    //se il numero è divisibile sia per 3 che per 5 (and), allora
+    if ((y % 3 == 0) && (y % 5 == 0)) {
+
+        //stampa "FizzBuzz"
+        console.log("FizzBuzz");
+
+    //altrimenti, se è divisibile per 3 ma NON per 5, allora
+    } else if ((y % 3 == 0) && (y % 5 != 0)) {
+        //stampa solo "Fizz"
+        console.log("Fizz");
+
+    //altriment, se NON è divisibile per 3 ma solo per 5, allora
+    } else if ((y % 3 != 0) && (y % 5 == 0)) {
+        //stampa solo "Buzz"
+        console.log("Buzz");
+    //se non rientra in nessuna delle condizioni sopra, allora
+    } else {
+        //stampa il valore dell'indice
+        console.log(y);
+    }
+
+    y++;
+
+} while (y < 100);
